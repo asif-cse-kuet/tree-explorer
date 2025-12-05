d8a73e1545edb893ec6ff48a6f82cc83f4486f52
tonmoy <git.sandbox3000.com>
2025-12-06 01:00:38 +0600

    Improt button modified

--- src/module/components/Tree.vue
+++ src/module/components/Tree.vue
@@ -38,7 +38,7 @@ provide('handleTreeDelete', handleTreeDeleteRequest);
           <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
           </svg>
-          Import JSON
+          Import
         </button>
       </div>
       
